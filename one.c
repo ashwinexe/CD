fac0a614512b27591db82ccb6bd439619fec8869
Ashwin Kumar Uppala <kumarashwin2603@gmail.com>
2022-03-30 06:03:57 +0000

    first commit

---
+++ one.c
@@ -0,0 +1,32 @@
+// 1)	Implement  DFA that accept the languages ending with “01” over the characters {0, 1} –
+
+#include <stdio.h>
+#define max 100
+
+int main(void) {
+    char str[max];
+    char f = 'a';
+    
+    printf("enter the string to be checked: ");
+    scanf("%s", str);
+    for(int i=0; str[i] != '\0'; i++){
+        switch(f){
+            case 'a': if(str[i] == '0') f = 'b';
+                        else if (str[i] == '1') f = 'a';
+                        break;
+            case 'b': if(str[i] == '0') f = 'b';
+                        else if (str[i] == '1') f = 'c';
+                        break;
+            // case 'c': if(str[i] == '0') f = 'b';
+            //             else if(str[i] == '1') f = 'a';
+            //             break;
+        }
+    }
+    if(f == 'c')
+        printf("String is accepted ", f);
+
+    else
+        printf("String is not accepted ", f);
+
+    return 0;
+}
